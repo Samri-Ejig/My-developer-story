f201eba23f08d293f76577955ba8e88b4f7afa94
Samrawit Debebe <102491692+Samri-Ejig@users.noreply.github.com>
2022-11-16 17:46:13 -0500

    weather add

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
-
+<script>
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(weatherwidget-io)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+</script>
 
 
